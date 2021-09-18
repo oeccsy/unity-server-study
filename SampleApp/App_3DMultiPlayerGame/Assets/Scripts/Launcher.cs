1c29825a8295cf2251091ba190c848af5ceb71b7
oeccsy <oeccsy@gmail.com>
2021-09-18 18:03:15 +0900

    Add Room Connection
    - Create Room
    - Leave Room

--- SampleApp/App_3DMultiPlayerGame/Assets/Scripts/Launcher.cs
+++ SampleApp/App_3DMultiPlayerGame/Assets/Scripts/Launcher.cs
@@ -8,6 +8,7 @@ public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] TMP_InputField roomNameInputField;
     [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
     // Start is called before the first frame update
     void Start()
     {
@@ -47,5 +48,23 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         errorText.text = "Room Createion Failed: " + message;
         MenuManager.Instance.OpenMenu("error");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("asdf");
+        MenuManager.Instance.OpenMenu("room");  //룸 메뉴를 연다.
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name; //들어간 방 이름
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom(); //방 떠나기 : 포톤 네트워크 기능이다.
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title"); //방 떠나기 성공시 타이틀 메뉴 호출
     }
 }
